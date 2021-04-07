599bcdd5eac2428caa135e73cba118bf7c1c0787
Victor Kutlak <vikutlak@gmail.com>
2021-04-07 15:34:13 -0300

    Chapter 02 Challenge 01 - Ignite - Solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user.admin === false) {
+      throw new Error("Only admins can get this information.");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,19 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    if (Array.isArray(user_id)) {
+      throw new Error("Id must be a string and not an array");
+    }
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
